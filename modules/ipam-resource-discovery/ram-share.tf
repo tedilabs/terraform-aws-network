41b60048e41dd6003c9ff13a361c10ca759e77a5
Byungjin Park <posquit0.bj@gmail.com>
2023-03-20 02:58:19 +0900

    Add ipam-resource-discovery module

---
+++ modules/ipam-resource-discovery/ram-share.tf
@@ -0,0 +1,32 @@
+###################################################
+# Resource Sharing by RAM (Resource Access Manager)
+###################################################
+
+module "share" {
+  source  = "tedilabs/account/aws//modules/ram-share"
+  version = "~> 0.24.0"
+
+  for_each = {
+    for share in var.shares :
+    share.name => share
+  }
+
+  name = "ipam.resource-discovery.${var.name}.${each.key}"
+
+  resources = [
+    aws_vpc_ipam_resource_discovery.this.arn
+  ]
+  permissions = each.value.permissions
+
+  external_principals_allowed = each.value.external_principals_allowed
+  principals                  = each.value.principals
+
+  resource_group_enabled = false
+  module_tags_enabled    = false
+
+  tags = merge(
+    local.module_tags,
+    var.tags,
+    each.value.tags,
+  )
+}
