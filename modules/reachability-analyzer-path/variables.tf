16b5171368249550be3976aa264f54857e981990
Byungjin Park <posquit0.bj@gmail.com>
2023-03-24 01:47:29 +0900

    Update reachability-analyzer-path

--- modules/reachability-analyzer-path/variables.tf
+++ modules/reachability-analyzer-path/variables.tf
@@ -19,7 +19,7 @@ variable "protocol" {
 variable "source_network" {
   description = <<EOF
   (Required) The configuration of source network for analysis. `source_network` as defined below.
-    (Required) `id` - The ID of resource which is the source of the path. Can be an Instance, Internet Gateway, Network Interface, Transit Gateway, VPC Endpoint, VPC Peering Connection or VPN Gateway.
+    (Required) `id` - The ID of resource which is the source of the path. Can be an Instance, Internet Gateway, Network Interface, Transit Gateway, Transit Gateway Attachment, VPC Endpoint Service, VPC Endpoint, VPC Peering Connection or VPN Gateway.
     (Optional) `ip_address` - The IP address of the source resource.
   EOF
   type = object({
@@ -32,7 +32,7 @@ variable "source_network" {
 variable "destination_network" {
   description = <<EOF
   (Required) The configuration of destination network for analysis. `destination_network` as defined below.
-    (Required) `id` - The ID of resource which is the destination of the path. Can be an Instance, Internet Gateway, Network Interface, Transit Gateway, VPC Endpoint, VPC Peering Connection or VPN Gateway.
+    (Required) `id` - The ID of resource which is the destination of the path. Can be an Instance, Internet Gateway, Network Interface, Transit Gateway, Transit Gateway Attachment, VPC Endpoint Service, VPC Endpoint, VPC Peering Connection or VPN Gateway.
     (Optional) `ip_address` - The IP address of the destination resource.
     (Optional) `port` - The port number of destination to analyze access to.
   EOF
@@ -48,7 +48,7 @@ variable "analyses" {
   description = <<EOF
   (Optional) The configuration of analyses to run with the reachability analyzer path. Each block of `analyses` as defined below.
     (Required) `name` - A name of the analysis with the reachability analyzer path.
-    (Optional) `required_intermediate_components` - A list of ARNs for resources the path must traverse. Intermediate components include load balancers, NAT gateways, and peering connections. You cannot use security groups, network access control lists, network interfaces, or route tables as intermediate components.
+    (Optional) `required_intermediate_components` - A list of ARNs for resources the path must traverse. Intermediate components include Load Balancers, NAT Gateways, and Network Firewall, Transit Gateways, Transit Gateway Attachments, VPC Peering Connections. You cannot use security groups, network access control lists, network interfaces, or route tables as intermediate components.
     (Optional) `wait_for_completion` - Whether to wait for the analysis status to change to `succeeded` or `failed`. Setting this to `false` will skip the process. Defaults to `true`.
   EOF
   type = list(object({
