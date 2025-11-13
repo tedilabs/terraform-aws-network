4a6c405f8e1f231f617680853afbeefea74df01c
Byungjin Park <posquit0.bj@gmail.com>
2025-11-14 02:17:00 +0900

    fix(subnet-group): update ram-share module

--- modules/subnet-group/ram-share.tf
+++ modules/subnet-group/ram-share.tf
@@ -1,10 +1,19 @@
+locals {
+  ram_share_name_prefix = join(".", [
+    "vpc",
+    "subnet-group",
+    replace(var.name, "/[^a-zA-Z0-9_\\.-]/", "-"),
+  ])
+}
+
+
 ###################################################
 # Resource Sharing by RAM (Resource Access Manager)
 ###################################################
 
 module "share" {
   source  = "tedilabs/organization/aws//modules/ram-share"
-  version = "~> 0.4.0"
+  version = "~> 0.5.0"
 
   for_each = {
     for share in var.shares :
@@ -13,9 +22,12 @@ module "share" {
 
   region = values(aws_subnet.this)[0].region
 
-  name = "vpc.subnet-group.${var.name}.${each.key}"
+  name = "${local.ram_share_name_prefix}.${each.key}"
 
-  resources = values(aws_subnet.this)[*].arn
+  resources = {
+    for name, subnet in aws_subnet.this :
+    name => subnet.arn
+  }
 
   permissions = each.value.permissions
 
